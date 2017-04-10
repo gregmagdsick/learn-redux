72b6fd11097ae08bdfc4fe9765e704b389f6e252
Greg Magdsick <greg.magdsick@gmail.com>
2017-04-10 12:05:34 -0700

    adding .babelrc and .eslintrc files

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//import css
+import css from './styles/style.styl';
+
+render(<p>himy</p>, document.getElementById('root'));
